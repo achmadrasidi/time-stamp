6238a4d8dc17d48bc63208b678d400750186bc3c
Achmad Rasidi <achmadrasidi@gmail.com>
2022-03-11 21:40:48 +0700

    Add files via upload

---
+++ server.js
@@ -0,0 +1,45 @@
+// server.js
+// where your node app starts
+
+// init project
+var express = require("express");
+var app = express();
+
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC
+var cors = require("cors");
+app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static("public"));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/views/index.html");
+});
+
+// your first API endpoint...
+app.get("/api/:date?", function (req, res) {
+  const inputDate = req.params.date;
+  let date;
+
+  if (!inputDate) {
+    date = new Date();
+  } else {
+    const checkUnix = inputDate * 1;
+    date = isNaN(checkUnix) ? new Date(inputDate) : new Date(checkUnix);
+  }
+
+  if (date === "Invalid Date") {
+    res.json({ error: "Invalid Date" });
+  } else {
+    const unix = date.getTime();
+    const utc = date.toUTCString();
+    res.json({ unix, utc });
+  }
+});
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function () {
+  console.log("Your app is listening on port " + listener.address().port);
+});
